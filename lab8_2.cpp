dea54886fdbf37cb5a8d7fad2f93db913dd4ec4d
Your Name <you@example.com>
2022-12-20 16:26:12 +0700

    TIT4NX Lab8_2

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,18 +1,55 @@
-//Dialog of the first example is given below.
+#include <iostream>
+#include <cmath>
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+using namespace std;
+
+int main()
+{
+    string Username;
+    
+    cout << "Fahsai: Sawadee ka...Can you tell me your name? " <<"\n";
+    
+    cout << "?????: " ;
+    getline(cin, Username );
+    
+    cout << "Fahsai: Wow!!! "<< Username << " is a really cool name." <<"\n";
+    
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID? "<<"\n";
+    
+    int UserID;
+    cout << Username <<": " ;
+    cin >> UserID;
+    cin.ignore();
+    
+    int Gear = (UserID/10000000)-12;
+
+    cout <<"Fahsai: I think you may be GEAR " << Gear <<". "<< "I have a free movie ticket for you." <<"\n";
+    
+    cout << "Fahsai: Let's go to the cinema together!!!" <<"\n";
+    
+    cout << "Fahsai: What movie do you want to watch?" <<"\n";
+    
+    string Movie;    
+    cout << Username << ": ";
+    getline(cin , Movie);
+    
+    cout <<"Fahsai: So....which day are you free to go with me?" <<"\n";
+    
+    string Day;
+    cout << Username << ": ";
+    getline(cin , Day);
+    
+    cout <<"Fahsai: "<< Day <<"....that is OK!!! I'm looking forward to watching " << Movie << " with you."<<"\n";
+    
+    string Text;
+    cout << Username << ": ";
+    getline(cin , Text);
+    
+    cout << "Fahsai: 555+ see you "<< Day <<"."<< " Bye Bye \\(^ ^)/";
+    
+    
+    return 0;
+}
+
+
+
